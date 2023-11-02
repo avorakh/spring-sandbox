45d685696756e836a1f9f0a27ca26439f456bf4d
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2023-11-02 11:33:22 +0100

    DCP-23: add Generated annotation to exclude from jacoco report

--- spring-web/src/test/java/by/avorakh/sandbox/spring/web/controller/HelloControllerTest.java
+++ spring-web/src/test/java/by/avorakh/sandbox/spring/web/controller/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package by.avorakh.sandbox.spring.web;
+package by.avorakh.sandbox.spring.web.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
