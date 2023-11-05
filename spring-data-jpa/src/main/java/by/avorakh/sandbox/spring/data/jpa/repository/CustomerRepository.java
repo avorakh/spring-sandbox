08f679ec9a2bedf20039e015adb0df4d1e7e1730
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2023-11-05 14:15:40 +0100

    DCP-25: add Spring data module

---
+++ spring-data-jpa/src/main/java/by/avorakh/sandbox/spring/data/jpa/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package by.avorakh.sandbox.spring.data.jpa.repository;
+
+import by.avorakh.sandbox.spring.data.jpa.entity.Customer;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
